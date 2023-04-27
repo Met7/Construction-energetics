fc848bb981b9290d48c5e292b6fa07dc021c5435
Dominik <dominik.franek@gmail.com>
2023-04-27 22:22:59 +0200

    Adding a new job works

--- script.js
+++ script.js
@@ -1,116 +1,96 @@
-// Add event listener to add job button
-document.getElementById("addJobBtn").addEventListener("click", addJob);
+function calculateJobEnergy(job) {
+  const materialSelect = job.querySelector(".material-select");
+  const materialOption = materialSelect.options[materialSelect.selectedIndex];
+  const material = materialOption.value;
+  const materialQuantity = parseFloat(job.querySelector(".material-quantity").value);
 
-// Function to add a new job container
-function addJob() {
-  // Get the job container template
-  const template = document.getElementById("jobTemplate");
-  // Create a new job container by cloning the template
-  const newJob = template.content.cloneNode(true);
-  // Append the new job container to the end of the job list
-  document.getElementById("jobList").appendChild(newJob);
-
-  // Set up event listener for calculate button
-  const calculateBtn = newJob.querySelector(".calculateBtn");
-  calculateBtn.addEventListener("click", calculateEnergy);
-}
-
-// Function to calculate the total energy for the job
-function calculateEnergy() {
-  // Get the stage 1 material and quantity
-  const stage1Material = this.closest(".jobContainer").querySelector(".stage1Material").value;
-  const stage1Quantity = parseFloat(this.closest(".jobContainer").querySelector(".stage1Quantity").value);
-
-  // Calculate the base energy for stage 2 based on the stage 1 material
-  let stage2BaseEnergy = 0;
-  switch (stage1Material) {
+  const collectionEnergyLabel = job.querySelector(".collection-energy-label");
+  let collectionEnergy = 0;
+  switch (material) {
     case "timber":
-      stage2BaseEnergy = 50;
+      collectionEnergy = 50;
       break;
     case "dirt":
-      stage2BaseEnergy = 40;
+      collectionEnergy = 40;
       break;
     case "limestone":
     case "granite":
-      stage2BaseEnergy = 500;
+      collectionEnergy = 500;
       break;
-    default:
-      console.error("Invalid stage 1 material: " + stage1Material);
-      return;
   }
+  collectionEnergyLabel.textContent = collectionEnergy;
 
-  // Get the stage 2 collection technology and calculate the energy modifier
-  const stage2CollectionTech = parseFloat(this.closest(".jobContainer").querySelector(".stage2CollectionTech").value);
-  const stage2EnergyModifier = stage2BaseEnergy / stage2CollectionTech;
+  const collectionMethodSelect = job.querySelector(".collection-method-select");
+  const collectionMethodOption = collectionMethodSelect.options[collectionMethodSelect.selectedIndex];
+  const collectionMethodModifier = parseFloat(collectionMethodOption.getAttribute("data-energy-modifier"));
+  const collectionMethodEnergy = collectionEnergy * collectionMethodModifier;
 
-  // Get the stage 3 transportation method and distance
-  const stage3TransportMethod = parseFloat(this.closest(".jobContainer").querySelector(".stage3TransportMethod").value);
-  const stage3Distance = parseFloat(this.closest(".jobContainer").querySelector(".stage3Distance").value);
+  const transportationSelect = job.querySelector(".transportation-select");
+  const transportationOption = transportationSelect.options[transportationSelect.selectedIndex];
+  const transportationEnergy = parseFloat(transportationOption.getAttribute("data-energy-per-ton-per-meter"));
+  const transportationDistance = parseFloat(job.querySelector(".transportation-distance").value);
+  const transportationWeight = materialQuantity * 1000;
+  const transportationEnergyTotal = transportationEnergy * transportationDistance * transportationWeight;
 
-  // Calculate the weight based on the material and quantity
-  let weight = 0;
-  switch (stage1Material) {
-    case "timber":
-      weight = stage1Quantity * 600;
-      break;
-    case "dirt":
-      weight = stage1Quantity * 1800;
-      break;
-    case "limestone":
-      weight = stage1Quantity * 2700;
-      break;
-    case "granite":
-      weight = stage1Quantity * 2900;
-      break;
-    default:
-      console.error("Invalid stage 1 material: " + stage1Material);
-      return;
-  }
+  const processingSelect = job.querySelector(".processing-select");
+  const processingOption = processingSelect.options[processingSelect.selectedIndex];
+  const processingEnergyModifier = parseFloat(processingOption.getAttribute("data-energy-modifier"));
 
-  // Calculate the energy for stage 3 based on the weight, distance, and transportation method
-  const stage3Energy = weight * stage3Distance * stage3TransportMethod;
+  const placingSelect = job.querySelector(".placing-select");
+  const placingOption = placingSelect.options[placingSelect.selectedIndex];
+  const placingEnergyModifier = parseFloat(placingOption.getAttribute("data-energy-modifier"));
 
-  // Calculate the total energy for the job
-  const totalEnergy = stage1Quantity * stage2EnergyModifier + stage3Energy;
+  const totalEnergy = collectionMethodEnergy + transportationEnergyTotal + processingEnergyModifier + placingEnergyModifier;
 
-  // Output the total energy to the job container
-  const output = this.closest(".jobContainer").querySelector(".output");
-  output.textContent = "Total energy: " + totalEnergy.toFixed(2) + " kJ";
+  job.querySelector(".total-energy").textContent = totalEnergy.toFixed(2);
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const jobContainer = document.querySelector(".job-container");
+  const addJobButton = document.querySelector("#add-job-button");
 
+  addJobButton.addEventListener("click", () => {
+    //const lastJob = jobContainer.querySelector(".job:last-child");
+    //const newJob = lastJob.cloneNode(true);
+    const newJob = jobContainer.cloneNode(true);
+    
+    // Get the job number
+    const jobNumber = document.querySelectorAll(".job-container").length + 1;
 
-// // Get the button element
-// const addJobBtn = document.getElementById("add-job-btn");
+    // Set the job number in the new job container
+    newJob.querySelector("h2").textContent = `Job ${jobNumber}`;
+    
+    // Set the ID of the materials input element
+    newJob.querySelector("label[for='materials-1']").setAttribute("for", `materials-${jobNumber}`);
+    newJob.querySelector("#materials-1").setAttribute("id", `materials-${jobNumber}`);
 
-// // Get the job container element
-// const jobContainer = document.querySelector(".job-container");
+    // Set the ID of the quantity input element
+    newJob.querySelector("label[for='quantity-1']").setAttribute("for", `quantity-${jobNumber}`);
+    newJob.querySelector("#quantity-1").setAttribute("id", `quantity-${jobNumber}`);
 
-// // Add event listener to button
-// addJobBtn.addEventListener("click", function () {
-  // // Clone the job container element
-  // const newJobContainer = jobContainer.cloneNode(true);
+    // Set the ID of the total-energy span element
+    newJob.querySelector("#total-energy-1").setAttribute("id", `total-energy-${jobNumber}`);
 
-  // // Get the job number
-  // const jobNumber = document.querySelectorAll(".job-container").length + 1;
+    // Set the total-energy to 0
+    newJob.querySelector(`#total-energy-${jobNumber}`).textContent = "0";
 
-  // // Set the job number in the new job container
-  // newJobContainer.querySelector("h2").textContent = `Job ${jobNumber}`;
+    
+    // newJob.querySelector(".material-select").selectedIndex = 0;
+    // newJob.querySelector(".collection-method-select").selectedIndex = 0;
+    // newJob.querySelector(".transportation-select").selectedIndex = 0;
+    // newJob.querySelector(".processing-select").selectedIndex = 0;
+    // newJob.querySelector(".placing-select").selectedIndex = 0;
+    // newJob.querySelector(".total-energy").textContent = "0.00";
+    jobContainer.appendChild(newJob);
+  });
 
-  // // Set the ID of the materials input element
-  // newJobContainer.querySelector("label[for='materials-1']").setAttribute("for", `materials-${jobNumber}`);
-  // newJobContainer.querySelector("#materials-1").setAttribute("id", `materials-${jobNumber}`);
+  jobContainer.addEventListener("input", (event) => {
+    if (event.target.matches(".job input, .job select")) {
+      const job = event.target.closest(".job");
+      calculateJobEnergy(job);
+    }
+  });
 
-  // // Set the ID of the labor input element
-  // newJobContainer.querySelector("label[for='labor-1']").setAttribute("for", `labor-${jobNumber}`);
-  // newJobContainer.querySelector("#labor-1").setAttribute("id", `labor-${jobNumber}`);
-
-  // // Set the ID of the man-hours span element
-  // newJobContainer.querySelector("#man-hours-1").setAttribute("id", `man-hours-${jobNumber}`);
-
-  // // Set the man-hours to 0
-  // newJobContainer.querySelector(`#man-hours-${jobNumber}`).textContent = "0";
-
-  // // Append the new job container to the job container element
-  // jobContainer.parentNode.appendChild(newJobContainer);
-// });
+  const firstJob = jobContainer.querySelector(".job");
+  calculateJobEnergy(firstJob);
+});
